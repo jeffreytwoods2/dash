6b5ff32eda4c4d6a33e7a1d29f326cd04ce5538a
Jeffrey Woods <11024584@uvu.edu>
2024-01-10 15:56:44 -0700

    Formatted all UI

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.onload = () => {
     let menu = document.getElementById("menu-btn")
     let nav = document.getElementById("dropdown")
+    let main = document.querySelector("main")
 
     menu.addEventListener("click", () => {
         nav.classList.toggle("menu-open")
+        main.classList.toggle("blur")
     })
 }
