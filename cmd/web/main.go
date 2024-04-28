75653325d70383534016b29ef21a827e3d5b7c78
jeffreywoods <jeffreytwoods2@icloud.com>
2024-04-27 22:02:12 -0600

    Changed default port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,7 +14,7 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":5001", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
